e61b0d6a0accfbebe625054b2fc096cbf94c84f7
vetalcore <vetalcore@gmail.com>
2014-10-31 09:06:03 +0200

    updated css

--- controllers/controller.js
+++ controllers/controller.js
@@ -13,116 +13,180 @@
 (function(){
 	angular.module('app', []);
 
-	function mainCtrl(boardObject, gameObject, Checker){
+	function mainCtrl(boardObject, gameObject, Checkers, King){
 		var game = this;
 		game.board = boardObject.board;
 		game.score = gameObject.score.boardState;
-		game.players =  gameObject.players;
-		game.gameScore = gameObject.score.boardScore;
-
-		game.hover = function(a,e){
-			//console.log(a);
-			//console.log(document.querySelectorAll('.some-directive:nth-of-type('+a+'),.some-directive:nth-of-type('+(a+1)+')'));
+		game.walking = {
+			color: 'white',
+			checkerMustGo : false,
+		};
+		game.res = {
+			white: 12,
+			black:12
+		};
+		// game.hover = function(a,e){
+		// 	//console.log(a);
+		// 	//console.log(document.querySelectorAll('.some-directive:nth-of-type('+a+'),.some-directive:nth-of-type('+(a+1)+')'));
 			
-		}
+		// }
 
-		game.leave = function(a,e){
+		// game.leave = function(a,e){
 			
-		}
+		// }
 
-		game.chooseSide = function(event){
-			console.log(event.target.value);
-		}
+		// game.chooseSide = function(event){
+		// 	console.log(event.target.value);
+		// }
 
 		game.makeTurn = function(event){
 			
-			
-
-
+			console.log('in start');
 			var elementIndex = Array.prototype.indexOf.call(event.target.parentElement.children, event.target);
 			console.log(elementIndex);
 			
 			
 			game.score.turns = gameObject.score.turns;
 
-			if(game.score[elementIndex]) {
+			if(game.score[elementIndex] && game.score[elementIndex].color == game.walking.color) {
 				console.log('in select');
-				if( 'x' in window && Checker.possibleMoves.length ) {
-				 	Array.prototype.forEach.call(document.querySelectorAll( Checker.possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){ x.style.opacity = 1;});
-					Checker.clearPossibleMoves();
+				if( 'x' in window && game.score[elementIndex].possibleMoves.length ) {
+				 	Array.prototype.forEach.call(document.querySelectorAll( game.score[elementIndex].possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){ x.style.background = 'none';});
+					game.score[elementIndex].clearPossibleMoves();
 				}
 				x = elementIndex; 
-				Checker.clearPossibleMoves();
-				// arr = [];
-				//console.log('set x:'+ x ); 
+				game.score[elementIndex].clearPossibleMoves();
 				//[elementIndex+7, elementIndex+9, elementIndex-7, elementIndex-9].forEach(function(x,i){if(!game.score[x] && x > elementIndex){arr.push(x); return false;} else if(game.score[x] && game.score[x] !== game.score[elementIndex] && !game.score[x+x-elementIndex]) arr.push(x+x-elementIndex); })
-				//Array.prototype.forEach.call(document.querySelectorAll('.some-directive:nth-of-type('+(game.score[x] == 'x' ? (game.score[x-9] == 'x' ? -1 : x-8) : (game.score[x+9] == 'o' ? -1 : x+10))+'),.some-directive:nth-of-type('+(game.score[x] == 'x' ? (game.score[x-7] == 'x' ? -1 : x-6) : (game.score[x+7] == 'o' ? -1 : x+8))+')'),function(x){x.style.opacity = 0;});
+				//Array.prototype.forEach.call(document.querySelectorAll('.some-directive:nth-of-type('+(game.score[x] == 'x' ? (game.score[x-9] == 'x' ? -1 : x-8) : (game.score[x+9] == 'o' ? -1 : x+10))+'),.some-directive:nth-of-type('+(game.score[x] == 'x' ? (game.score[x-7] == 'x' ? -1 : x-6) : (game.score[x+7] == 'o' ? -1 : x+8))+')'),function(x){x.style.background = 'red';});
 				//console.log(game.score[elementIndex],x, x+9,game.score[x+9],x+7,game.score[x+7]);
-				Checker.setPossibleMoves(elementIndex, game.score);
-				if( Checker.possibleMoves.length )Array.prototype.forEach.call(document.querySelectorAll( Checker.possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){ x.style.opacity = 0;});
+				game.score[elementIndex].setPossibleMoves(elementIndex, game.score);
+				//console.log(game.score[elementIndex].possibleMoves ); 
+				if( game.score[elementIndex].possibleMoves.length )Array.prototype.forEach.call(document.querySelectorAll( game.score[elementIndex].possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){ x.style.background = 'red';});
 				//console.log(Checker.possibleMoves);
+				//console.log('posiible:',game.score[x].possibleMoves);
 
 			}
-			else if('x' in window && x && Checker.possibleMoves.indexOf(elementIndex) > -1) { 
-				console.log('in move');
-				Array.prototype.forEach.call(document.querySelectorAll( Checker.possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){console.log(x); x.style.opacity = 1;});
-				//console.log([x+8, x+6]);
+			else if('x' in window && x && game.score[x].possibleMoves.indexOf(elementIndex) > -1) { 
+				console.log('in move: ', elementIndex,' , ', x);
+				Array.prototype.forEach.call(document.querySelectorAll( game.score[x].possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){console.log(x); x.style.background = 'none';});
+				
+				//console.log('posiible:',game.score[x].possibleMoves);//console.log([x+8, x+6]);
 				game.score[elementIndex] = game.score.splice(x, 1, game.score[elementIndex])[0];
-				if([7, 9, -7, -9].indexOf(elementIndex-x)/2 > -1) game.score[x + (elementIndex-x)/2] = '';
+				if([7, 9, -7, -9].indexOf((elementIndex-x)/2) > -1) { 
+					console.log('in beat');
+					game.score[x + (elementIndex-x)/2] = '';
+					game.score[elementIndex].setPossibleMoves(elementIndex, game.score);
+					var counts = {};
+					//console.log()
+					game.score.forEach(function(element){ if(element)counts[element.color] = (counts[element.color] || 0) + 1;});
+					game.res = counts;
+					//console.log(counts)
+					//console.log(game.score[elementIndex].possibleMoves);
+					if(game.score[elementIndex].possibleMoves.length) {
+						if(game.score[x].possibleMoves.length) Array.prototype.forEach.call(document.querySelectorAll( game.score[x].possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){console.log(x); x.style.background = 'none';});
+						Array.prototype.forEach.call(document.querySelectorAll( game.score[elementIndex].possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){console.log(x); x.style.background = 'red';});
+						game.walking.checkerMustGo = elementIndex;
+					}
+					else { 
+						game.walking.color = ['black', 'white'][['white', 'black'].indexOf(game.walking.color)];
+					}
+				}
+				else {
+					game.walking.color = ['black', 'white'][['white', 'black'].indexOf(game.walking.color)];
+				}
+
+				
+
+				//game.score[2] = King.getKingInstance(Checkers.getCheckerInstance('sraka'));
+				//console.log(game.score[0].color, x);
 				//console.log((elementIndex-x)/2 in [7, 9, -7, -9],(elementIndex-x)/2);
-				//Array.prototype.forEach.call(document.querySelectorAll('.some-directive:nth-of-type('+(game.score[elementIndex] == 'x' ? x-8 : x+10)+'),.some-directive:nth-of-type('+(game.score[elementIndex] == 'x' ? x-6 : x+8)+')'),function(x){x.style.opacity = 1;});
+				//Array.prototype.forEach.call(document.querySelectorAll('.some-directive:nth-of-type('+(game.score[elementIndex] == 'x' ? x-8 : x+10)+'),.some-directive:nth-of-type('+(game.score[elementIndex] == 'x' ? x-6 : x+8)+')'),function(x){x.style.background = 'none';});
 				x = 0;
+				//console.log('posiible:',game.score[x].possibleMoves);
 			}
-			else if( 'x' in window && Checker.possibleMoves.length ) {
-				 	Array.prototype.forEach.call(document.querySelectorAll( Checker.possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){ x.style.opacity = 1;});
-					Checker.clearPossibleMoves();
+			else if( 'x' in window && x && game.score[x].possibleMoves.length ) {
+				console.log('in end');
+				//console.log(x);
+				 Array.prototype.forEach.call(document.querySelectorAll( game.score[x].possibleMoves.reduce(function(a,b){ return a+'.some-directive:nth-of-type('+(b+1)+'),';},'').slice(0, -1)),function(x){ x.style.background = 'none';});
+				game.score[x].clearPossibleMoves();
 			}
 		}
 	}
-
-	function Checker(){
-		var self = this;
-		self.color = {
-			o : {color: 'white', direction: -1},
-			x : {color: 'black', direction: 1},
-		};		
-		self.possibleMoves = [];
-		self.setPossibleMoves = function(index, board){	
-			console.log((index/Math.sqrt(board.length+1) | 0),self.color[board[index]].direction);
-			[index+7, index+9, index-7, index-9].forEach(function(x,i){if(!board[x] && (index - x) * self.color[board[index]].direction >= 0 && (index/Math.sqrt(board.length+1) | 0)  == (x/Math.sqrt(board.length+1) | 0) + self.color[board[index]].direction){self.possibleMoves.push(x); return false;} else if(board[x] && board[x] !== board[index] && !board[x+x-index] && [((x+x-index)/Math.sqrt(board.length+1) | 0) + 2*self.color[board[index]].direction, ((x+x-index)/Math.sqrt(board.length+1) | 0) - 2*self.color[board[index]].direction].indexOf((index/Math.sqrt(board.length+1) | 0)) > -1) self.possibleMoves.push(x+x-index); });
-		 };
-		self.clearPossibleMoves = function(){ self.possibleMoves = []; };
-		self.getBeatenCheckers = function(){};
-		return self; 
+	mainCtrl.$inject = ['boardObject', 'gameObject', 'Checkers', 'King'];
+	function Checkers(){		
+		this.getCheckerInstance = function(color){
+			function Checker(color){
+				this.color = color;
+			}
+			Checker.prototype.figure = color == 'white' ? 'o' : 'x';
+			Checker.prototype.direction = color == 'white' ?  -1 : 1;
+			Checker.prototype.possibleMoves = [];
+			Checker.prototype.setPossibleMoves = function(index, board){
+				//console.log(board);
+			    var self = this;
+				[index+7, index+9, index-7, index-9].forEach(function(x,i){if(!board[x] && ((index - x) * self.direction) >= 0 && (index/Math.sqrt(board.length+1) | 0)  == (x/Math.sqrt(board.length+1) | 0) + self.direction){self.possibleMoves.push(x); console.log('push x:',x); return false; } else if(board[x] && 'color' in board[x] && board[x].color !== board[index].color && !board[x+x-index] && [((x+x-index)/Math.sqrt(board.length+1) | 0) + 2*self.direction, ((x+x-index)/Math.sqrt(board.length+1) | 0) - 2*self.direction].indexOf((index/Math.sqrt(board.length+1) | 0)) > -1){ self.possibleMoves.push(x+x-index);console.log('push x+x-index:',x+x-index); } });
+			}
+			Checker.prototype.clearPossibleMoves = function(){ this.possibleMoves = []; }
+			Checker.prototype.getBeatenCheckers = function(){}
+			return new Checker(color);
+		}
+		return this;
 	}
-
-	function King(){
-
-	}
-	King.prototype = Object.create(Checker.prototype);
-
-	mainCtrl.prototype.someObj = { };
-	mainCtrl.$inject = ['boardObject', 'gameObject', 'Checker'];
-
-	function otherCtrl(){}
-	otherCtrl.prototype = Object.create(mainCtrl.prototype);
 	
-	function firstService(){
-		this.methods = function(){
-
-		};
+	function King(){		
+		this.getKingInstance = function(checker){
+			function King(){
+				//return Object.create(checker);
+			}
+			King.prototype = Object.create(checker);
+			// King.prototype.direction = [-1,1];
+			King.prototype.setPossibleMoves = function(index, board){
+				//console.log(board.map(function(x,i){return i; }));
+				console.log(board.map(function(x,i){return i; }));
+			    var self = this;
+				self.possibleMoves = board.map(function(x,i){return i; }).filter(function(a,k){ return [7,-7].some(function(y){return !((a - index)%y) && (a%Math.sqrt(board.length+1)) * (-1) >= (a%Math.sqrt(board.length+1) - index%Math.sqrt(board.length+1)) * (Math.sqrt(board.length+1) - Math.abs(y)) ;}); });
+			} 
+			return new King(checker);
+		}
+		return this;
 	}
 
-	function gameObject(){
+	// function LocalStorageObject(){
+	// 	// this.LSObject = {
+	// 	// 	header :
+	// 	// }
+	// 	this.lololo = [
+	// 	{header: 		}
+	// 	];
+	// }
+
+	// function King(){
+
+	// }
+	// King.prototype = Object.create(Checkers.prototype);
+
+	// mainCtrl.prototype.someObj = { };
+	
+
+	// function otherCtrl(){}
+	// otherCtrl.prototype = Object.create(mainCtrl.prototype);
+	
+	// function firstService(){
+	// 	this.methods = function(){
+
+	// 	};
+	// }
+
+	function gameObject(Checkers){
 		this.score = {
-		 	boardState : ['','o','','o','','o','','o','o','','o','','o','','o','','','o','','o','','o','','o','','','','','','','','','','','','','','','','','x','','x','','x','','x','','','x','','x','','x','','x','x','','x','','x','','x'],
+		 	boardState : ['',Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),'','',Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),'',Checkers.getCheckerInstance('white'),'','','','','','','','','','','','','','','','',Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black'),'','',Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black'),Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black'),'',Checkers.getCheckerInstance('black')],
 		 	boardScore : { 'x' : 0, 'o' : 0},
-		 	turns : 0
+		 	turns : 0,
 		}
 		this.players  = { 0: 'x', 1:'o'};
 		return this;
 	}
-
+	this.$inject = ['Checkers'];
 	function boardObject(gameObject){
 		var obj = {};
 		obj.boardLength;
@@ -140,36 +204,34 @@
 	        transclude: true,
 	        link: function (scope, element, attrs) {
 	        	element.on('click', function () {
-		      		var self = this;
-		        	console.log();
-		        	//self.innerHTML = gameObject.players[gameObject.turns%2];
 		      	});
 		    },
 			template: [
 			  	'<div class="some-directive">',
-			  	'{{game.score[$index]}}',
+			  	'<span content class="{{game.score[$index].color}}"></span>',
 			    '</div>'
 			].join('')
 		};
 	}
 	someDirective.$inject = ['gameObject'];
-	function someFilter(){
-		return function(items){
-			return items.filter(function(item){
-				return /^a/i.test(item.name);
-			});
-		};
-	}
+	// function someFilter(){
+	// 	return function(items){
+	// 		return items.filter(function(item){
+	// 			return /^a/i.test(item.name);
+	// 		});
+	// 	};
+	// }
 
 	angular
 		.module('app',[])
 		.controller('mainCtrl', mainCtrl)
-		.controller('otherCtrl', otherCtrl)
+		// .controller('otherCtrl', otherCtrl)
 		.factory('boardObject', boardObject)
 		.factory('gameObject', gameObject)
-		.factory('Checker', Checker)
-		.directive('someDirective', someDirective)
-		.filter('someFiler', someFilter);
+		.factory('Checkers', Checkers)
+		.factory('King', King)
+		.directive('someDirective', someDirective);
+		// .filter('someFiler', someFilter);
 })();
 
 
